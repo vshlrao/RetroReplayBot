dca9083cb8dbac6d0e9583b2974ab954f6660793
vshlrao <60248611+vshlrao@users.noreply.github.com>
2020-03-29 17:06:02 +1100

    wikipage history
    Use subreddit's wikipage to store history instead of text files

--- main.py
+++ main.py
@@ -13,43 +13,34 @@ subreddit = reddit.subreddit(config.subreddit)
 
 
 def main():
-    while True:
-        try:
-            # update with latest entries
-            rr_feed = feedparser.parse(config.feed)
-            latest_title = rr_feed['entries'][0]['title']
-            latest_link = rr_feed['entries'][0]['link']
+    try:
+        # update with latest entries
+        rr_feed = feedparser.parse(config.feed)
+        latest_title = rr_feed['entries'][0]['title']
+        latest_link = rr_feed['entries'][0]['link']
 
-            # get link of last uploaded/submitted video
-            file = open("latest_link.txt", "r")
+        # get history wikipage
+        history_wp = subreddit.wiki['uploadhistory']
 
-            # latest upload and latest submission not the same
-            if file.read() != latest_link:
-                # unsticky current top stickied post
-                top_sticky = subreddit.sticky(number=1)
-                top_sticky.mod.sticky(state=False)
+        # latest upload and latest submission not the same
+        if history_wp.content_md != latest_link:
+            # unsticky current top stickied post
+            top_sticky = subreddit.sticky(number=1)
+            top_sticky.mod.sticky(state=False)
 
-                # submit new video and sticky to top of subreddit
-                submission = subreddit.submit(latest_title, url=latest_link)
+            # submit new video and sticky to top of subreddit
+            submission = subreddit.submit(latest_title, url=latest_link)
 
-                # sticky [latest-1] submission to second sticky
-                submission.mod.sticky(state=True, bottom=False)
-                top_sticky.mod.sticky(bottom=True)
+            # sticky [latest-1] submission to second sticky
+            submission.mod.sticky(state=True, bottom=False)
+            top_sticky.mod.sticky(bottom=True)
 
-                # record latest submitted video's link
-                f_link = open("latest_link.txt", "w")
-                f_link.write(latest_link)
-                f_link.close()
-                f_title = open("latest_title.txt", "w")
-                f_title.write(latest_title)
-                f_title.close()
+            # change wiki page to latest video
+            # give title as reason for searchability
+            history_wp.edit(content=latest_link, reason=latest_title)
 
-            file.close()
-            time.sleep(300)
-        except Exception as e:
-            print(e)
-            break
-
+    except Exception as e:
+        reddit.redditor('supabatman').message('Bot Problem', e)
 
 
 if __name__ == "__main__":
